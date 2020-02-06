c62398732cdcd2d8100a19b64572b6d085e3b015
Peregon Bogdan <peregonbw@gmail.com>
2020-02-06 08:56:38 +0200

    deploy fix

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import {Mainpage} from "./components/Mainpage";
 import {Charts} from "./components/Charts/Charts";
 import {Route} from "react-router-dom";
 import {Users} from "./components/Users/Users";
+import {BrowserRouter} from "react-router-dom";
 
 class App extends React.Component {
 
 
     render() {
         return (
-            <div className="wrapper">
-                <Route path="/" exact render={() => <Mainpage/>}/>
-                <Route path="/users" render={() => <Users/>}/>
-                <Route path="/user/:userId?"
-                       render={(matchProps) => <Charts {...matchProps} handleMatch={this.handleMatch}/>}/>
-            </div>
+            <BrowserRouter>
+                <div className="wrapper">
+                    <Route path="/" exact render={() => <Mainpage/>}/>
+                    <Route path="/users" render={() => <Users/>}/>
+                    <Route path="/user/:userId?"
+                           render={(matchProps) => <Charts {...matchProps} handleMatch={this.handleMatch}/>}/>
+                </div>
+            </BrowserRouter>
         );
     }
 }
